117e9facdefcfa279b023c4a4f19e175895a9545
Charlie Zhang <blokaly@users.noreply.github.com>
2018-05-18 16:13:17 +0800

    influx db persister

--- services/stream-persister/src/main/java/com/blokaly/ceres/persist/TestPersister.java
+++ services/stream-persister/src/main/java/com/blokaly/ceres/persist/TestPersister.java
@@ -20,64 +20,78 @@ import java.util.concurrent.TimeUnit;
 
 @Singleton
 public class TestPersister extends AbstractIdleService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(TestPersister.class);
-    private final InfluxdbWriter writer;
-    private final Gson gson;
-    private final ScheduledExecutorService executor;
-    private final Map<String, String> tags;
-    private final Random rand = new Random();
-    private volatile boolean stop = false;
+  private static final Logger LOGGER = LoggerFactory.getLogger(TestPersister.class);
+  private final InfluxdbWriter writer;
+  private final Gson gson;
+  private final ScheduledExecutorService executor;
+  private final Map<String, String> tags;
+  private final Random rand = new Random();
+  private volatile boolean stop = false;
 
-    @Inject
-    public TestPersister(InfluxDB influxDB, Gson gson, @SingleThread ScheduledExecutorService executor) {
-        writer = new InfluxdbWriter(influxDB, "test");
-        this.gson = gson;
-        this.executor = executor;
-        tags = Maps.newHashMap();
-        tags.put("venue", "finfabrik");
-        tags.put("symbol", "btcusd");
+  @Inject
+  public TestPersister(InfluxDB influxDB, Gson gson, @SingleThread ScheduledExecutorService executor) {
+    writer = new InfluxdbWriter(influxDB, "test");
+    this.gson = gson;
+    this.executor = executor;
+    tags = Maps.newHashMap();
+    tags.put("venue", "finfabrik");
+    tags.put("symbol", "ethusd");
+  }
+
+
+  @Override
+  protected void startUp() throws Exception {
+    LOGGER.info("starting up");
+    executor.scheduleWithFixedDelay(this::persist, 1L, 1L, TimeUnit.SECONDS);
+  }
+
+  @Override
+  protected void shutDown() throws Exception {
+    LOGGER.info("shutting down");
+    stop = true;
+  }
+
+  private void persist() {
+    if (!stop) {
+      Point.Builder builder = Point.measurement("stream").time(System.nanoTime(), TimeUnit.NANOSECONDS).tag(tags);
+      boolean hasValue = false;
+      String bids = getLadders();
+      if (bids != null) {
+        builder.addField("bids", bids);
+        hasValue = true;
+      }
+      String asks = getLadders();
+      if (asks != null) {
+        builder.addField("asks", asks);
+        hasValue = true;
+      }
+
+      if (!hasValue) {
+        return;
+      }
+
+      try {
+        LOGGER.info("Persisting ladders to influxdb");
+        writer.write(builder.build());
+        LOGGER.info("Persisted");
+      } catch (Exception ex) {
+        LOGGER.error("Error writing to influxdb", ex);
+      }
     }
+  }
 
-
-    @Override
-    protected void startUp() throws Exception {
-        executor.schedule(this::persist, 1L, TimeUnit.SECONDS);
+  private String getLadders() {
+    if (rand.nextBoolean()) {
+      JsonArray ladders = new JsonArray();
+      for (int i = 0; i < 10; i++) {
+        JsonArray ladder = new JsonArray();
+        ladder.add(rand.nextDouble());
+        ladder.add(rand.nextDouble());
+        ladders.add(ladder);
+      }
+      return gson.toJson(ladders);
+    } else {
+      return null;
     }
-
-    @Override
-    protected void shutDown() throws Exception {
-        stop = true;
-    }
-
-    private void persist() {
-        if (!stop) {
-            Point.Builder builder = Point.measurement("stream").time(System.nanoTime(), TimeUnit.NANOSECONDS).tag(tags);
-            String bids = getLadders();
-            if (bids != null) {
-                builder.addField("bids", bids);
-            }
-            String asks = getLadders();
-            if (asks != null) {
-                builder.addField("asks", asks);
-            }
-            LOGGER.info("Persisting ladders to influxdb");
-            writer.write(builder.build());
-            executor.schedule(this::persist, 1L, TimeUnit.SECONDS);
-        }
-    }
-
-    private String getLadders() {
-        if (rand.nextBoolean()) {
-            JsonArray ladders = new JsonArray();
-            for (int i = 0; i < 5; i++) {
-                JsonArray ladder = new JsonArray();
-                ladder.add(rand.nextDouble());
-                ladder.add(rand.nextDouble());
-                ladders.add(ladder);
-            }
-            return gson.toJson(ladders);
-        } else {
-            return null;
-        }
-    }
+  }
 }
